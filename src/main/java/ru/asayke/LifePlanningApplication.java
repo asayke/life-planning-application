dd6da85815ce1962350395c6fd51dd76f48ab352
Кольдяжный Олег Евгеньевич <koldyazhnyi@vtb.ru>
2024-04-28 01:42:25 +0300

    Initial commit

---
+++ src/main/java/ru/asayke/LifePlanningApplication.java
@@ -0,0 +1,13 @@
+package ru.asayke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifePlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifePlanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/asayke/LifePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.asayke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifePlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
